8714b3ac61d40d32d051cf80d6d6f6271c81d7f0
Jakob Anderson <janderson@familysearch.org>
2017-10-02 11:32:34 -0600

    init

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send(204);
+});
+
+/* POST hayaku-beacon */
+router.post('/beacon', function(req, res, next) {
+  res.send(204);
+
+  // Parse beacon data
+
+  // Write log
+
+});
+
+module.exports = router;
